6c9963e60daa2961efef28cd0d08c92aa551e50b
Vinicius Correa Rocha <125145408+vncr07@users.noreply.github.com>
2023-05-17 20:13:28 -0300

    Update validacao.js

--- app/validacao.js
+++ app/validacao.js
@@ -5,6 +5,23 @@ function verificaSeOChutePossuiUmValorValido(chute) {
         elementoChute.innerHTML += '<div>valor invalido</div>'
         return
     }
+    
+    if (chuteForInvalido(numero)) {
+        if (chute.toUpperCase() === "GAME OVER") {
+
+            document.body.innerHTML =
+                `
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                `
+                document.body.style.backgroundColor = "black";
+        } else {
+
+            elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        }
+    }
+
 
     if (numeroForMaiorOuMenorQueOValorPermitido(numero)){
         elementoChute.innerHTML += `<div>valor invalido: Fale um numero entre ${menorValor} e ${maiorValor}</div>`
